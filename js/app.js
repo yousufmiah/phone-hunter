5a6db180ea65c77984c876d88991218c9091098a
yousufmiah <yousufweb99@gmail.com>
2022-03-01 20:37:38 +0600

    modal add

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
-//found data result======================
+//found data result=========================
 let dataCount = document.getElementById("found-data");
 let dataCountR = document.getElementById("found-dataR");
 let dataCountG = document.getElementById("found-dataG");
 
-//Spinner ================================
+//Spinner =================================
 const toggleSpinner = (displayStyle) => {
   document.getElementById("spinner").style.display = displayStyle;
 };
 
-// Search Button ===========================
+// Search Button ==========================
 const searchBtn = () => {
   document.getElementById("image-mobile").innerHTML = "";
   document.getElementById("mobile-details").innerHTML = "";
@@ -18,16 +18,16 @@ const searchBtn = () => {
   const searchBox = document.getElementById("search-box");
   const searchValue = searchBox.value;
 
-  // case to lower===============================
+  // case to lower========================
   const searchText = searchValue.toLowerCase();
   // console.log(searchText);
 
   searchBox.value = "";
 
   if (searchText == "") {
-    dataCountG.innerText = `Please Write Mobile Name !`;
+    dataCountG.innerText = `Please write your Mobile name !`;
   } else {
-    //spinner ======================
+    //spinner =============================
     toggleSpinner("block");
 
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
@@ -43,7 +43,7 @@ const searchBtn = () => {
     const phone = mobilePhone.slice(0, 20);
     //   console.log(mob);
 
-    //data count====================================
+    //data count==================================
     if (phone == 0) {
       dataCountR.innerText = `Results not found !`;
     } else if (phone.length > 1) {
@@ -65,7 +65,9 @@ const searchBtn = () => {
             <h5 class="card-title"><strong>Name:</strong> ${mobile.phone_name}</h5>
             <h6><strong>Brand:</strong> ${mobile.brand}</h6>
             <div class="card-footer bg-gray ">
-            <button class="btn btn-primary px-5 " onclick="mobileDetail('${mobile.slug}')">Details</button>
+          <button type="button" class="btn btn-primary px-5" data-bs-toggle="modal" 
+           data-bs-target="#exampleModal" onclick="modalCardDetail('${mobile.slug}')">
+           Details </button>
             </div>
           </div>
         </div>
@@ -74,62 +76,77 @@ const searchBtn = () => {
       imageMobile.appendChild(div);
     });
 
-    //spinner ======================
+    //spinner =======================================
     toggleSpinner("none");
   };
 };
 
 // Mobile details====================================
-const mobileDetail = (id) => {
+const modalCardDetail = (id) => {
   console.log(id);
   const url = `https://openapi.programming-hero.com/api/phone/${id}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => mobilePhoto(data));
+    .then((data) => modalBox(data));
 };
 
-const mobilePhoto = (mCard) => {
-  console.log(mCard.data);
-  document.getElementById("mobile-details").innerHTML = "";
-  const mobileDetails = document.getElementById("mobile-details");
+const modalBox = (mdCard) => {
+  // console.log(mCard.data);
+  document.getElementById("modal-Box").innerHTML = "";
+  const modalDeails = document.getElementById("modal-Box");
   const div = document.createElement("div");
-  div.classList.add("card", "p-2");
+  div.classList.add("modal-content");
   div.innerHTML = `
+
+  <div class="modal-header">
+  <h5 class="modal-title" id="exampleModalLabel">${mdCard.data.brand}</h5>
+  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+  </div>
+  <div class="modal-body">
   <div class="row g-0">
   <div class="col-md-4 text-center">
-  <img src="${mCard.data.image}" class="img-fluid rounded-start" alt="..." />
+  <img src="${mdCard.data.image}" class="img-fluid rounded-start" alt="..." />
 </div>
+
 <div class="col-md-8">
   <div class="card-body">
-    <h4 class="card-title"><strong>Name:</strong> ${mCard.data.name}</h4>
-    <h5 class="card-title"><strong>Brand:</strong> ${mCard.data.brand}</h5>
+    <h4 class="card-title"><strong>Name:</strong> ${mdCard.data.name}</h4>
+    <h5 class="card-title"><strong>Brand:</strong> ${mdCard.data.brand}</h5>
     <h6><strong>Release Date:</strong> ${
-      mCard.data.releaseDate ? mCard.data.releaseDate : "Not Available"
+      mdCard.data.releaseDate ? mdCard.data.releaseDate : "Not Available"
     }</h6>
     
-<small><strong>Main Features:</strong></small><br>
-<small><strong>ChipSet:</strong> ${mCard.data.mainFeatures.chipSet}</small><br>
-<small><strong>Display Size:</strong> ${
-    mCard.data.mainFeatures.displaySize
+  <small><strong>Main Features:</strong></small><br>
+  <small><strong>ChipSet:</strong> ${mdCard.data.mainFeatures.chipSet}</small>
+  <br>
+  <small><strong>Display Size:</strong> ${
+    mdCard.data.mainFeatures.displaySize
   }</small><br>
-<small><strong>Memory:</strong> ${mCard.data.mainFeatures.memory}</small> <br>
-<small><strong>Storage:</strong> ${mCard.data.mainFeatures.storage}</small> <br>
-<small><strong>Sensors:</strong> ${mCard.data.mainFeatures.sensors.join(
+  <small><strong>Memory:</strong> ${
+    mdCard.data.mainFeatures.memory
+  }</small> <br>
+  <small><strong>Storage:</strong> ${
+    mdCard.data.mainFeatures.storage
+  }</small> <br>
+  <small><strong>Sensors:</strong> ${mdCard.data.mainFeatures.sensors.join(
     ", "
   )}</small> <br>
-<br>
-<small><strong>Others:</strong> <br>
-<small><strong>Bluetooth:</strong> ${mCard.data.others?.Bluetooth}</small> <br>
-<small><strong>GPS:</strong> ${mCard.data.others?.GPS}</small> <br>
-<small><strong>NFC:</strong> ${mCard.data.others?.NFC}</small> <br>
-<small><strong>Radio:</strong> ${mCard.data.others?.Radio}</small> <br>
-<small><strong>USB:</strong> ${mCard.data.others?.USB}</small> <br>
-<small><strong>WLAN:</strong> ${mCard.data.others?.WLAN}</small> <br>
+  <br>
+  <small><strong>Others:</strong> <br> 
+  <small><strong>Bluetooth:</strong> ${
+    mdCard.data.others?.Bluetooth
+  }</small> <br>
+  <small><strong>GPS:</strong> ${mdCard.data.others?.GPS}</small> <br>
+  <small><strong>NFC:</strong> ${mdCard.data.others?.NFC}</small> <br>
+  <small><strong>Radio:</strong> ${mdCard.data.others?.Radio}</small> <br>
+  <small><strong>USB:</strong> ${mdCard.data.others?.USB}</small> <br>
+  <small><strong>WLAN:</strong> ${mdCard.data.others?.WLAN}</small> <br>
    
   </div>
   </div>
 </div>
-      `;
+</div>
 
-  mobileDetails.appendChild(div);
+`;
+  modalDeails.appendChild(div);
 };
